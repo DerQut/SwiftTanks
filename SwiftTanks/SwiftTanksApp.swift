afbd76c6033c381186329472f758e169ed8cbb21
Marcel Chołodecki <cholodecki@gmail.com>
2025-01-11 16:26:23 +0100

    Initial Commit

---
+++ SwiftTanks/SwiftTanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTanksApp.swift
+//  SwiftTanks
+//
+//  Created by Marcel Chołodecki on 11/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
